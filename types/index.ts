526ef91b620226739058bc1a407ed7b3b2325a0a
TMedalith <u20211f955@upc.edu.pe>
2025-01-14 13:49:56 -0500

    feat: Add Experience interface to define structure for experience data

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+
+export interface Experience {
+    id: number;
+    category: string;
+    title: string;
+    description: string;
+    image: string;
+    additionalImages: string[];
+    date: string;
+  }
