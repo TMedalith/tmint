8860edbcc338bbdcd851e413d7c34f90da59e9cc
TMedalith <u20211f955@upc.edu.pe>
2025-01-14 13:50:47 -0500

    feat: Extend Tailwind CSS configuration with new colors, fonts, animations, and keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,64 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'axolotl': '#588061',
+        'artichoke': '#869F77',
+        'linen': '#FDE9EA',
+        'pale-pink': '#F9E0DB',
+        'pale-chestnut': '#E7B5AC',
+        'text-description': 'var(--text-description)',
       },
+      fontFamily: {
+        'pacifico': ['Pacifico', 'cursive'],
+        'nunito': ['Nunito', 'sans-serif'],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      }, 
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
+        scroll:"scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+        twinkle: 'twinkle 3s infinite',
+        gradient: 'gradient 6s linear infinite',
+  
+      },
+      keyframes: {
+        twinkle: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0.3' },
+        },
+        gradient: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        shimmer: {
+          from: {
+            "backgroundPosition": "0 0"
+          },
+          to: {
+            "backgroundPosition": "-200% 0"
+          }
+        },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+
     },
   },
-  plugins: [],
 } satisfies Config;
